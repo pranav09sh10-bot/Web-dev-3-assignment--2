ebc8e05ad1cc3b4add9cd1517c869294f4b155a3
Pranav  kumar sharma <pranav09sh10@gmail.com>
2026-09-23 23:28:20 +0530

    Add student data to student.js

---
+++ student.js
@@ -0,0 +1,7 @@
+const students = [
+  { id: 1, name: "Aarav Harit", age: 20, course: "B.Tech CSE" },
+  { id: 2, name: "DEARS", age: 21, course: "BCA" },
+  { id: 3, name: "Zindagi", age: 19, course: "B.Sc IT" }
+];
+
+module.exports = students;
