44652262664b10daf6c3696aa879db9db59e9f48
Pranav  kumar sharma <pranav09sh10@gmail.com>
2026-09-23 23:46:05 +0530

    Add files via upload

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,7 +1,9 @@
 const logger = (req, res, next) => {
-  const time = new Date().toISOString();
-  console.log(`[${time}] ${req.method} ${req.url}`);
-  next();
+    console.log(
+        `${new Date().toISOString()} - ${req.method} ${req.url}`
+    );
+
+    next();
 };
 
 module.exports = logger;
