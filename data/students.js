99bb41ab19fde4c16f1d688c8997b7e3c7144e50
Pranav  kumar sharma <pranav09sh10@gmail.com>
2026-09-23 23:46:41 +0530

    Add files via upload

---
+++ data/students.js
@@ -0,0 +1,22 @@
+let students = [
+    {
+        id: 1,
+        name: "Yash",
+        age: 19,
+        course: "B.Tech CSE"
+    },
+    {
+        id: 2,
+        name: "Harsh",
+        age: 20,
+        course: "B.Tech CSE"
+    },
+    {
+        id: 3,
+        name: "Neha",
+        age: 19,
+        course: "B.Tech IT"
+    }
+];
+
+module.exports = students;
